8ad7affb7aad2d003a15dee864c97feb818b83c6
usernaimandrey <anshlyapnikov@yandex.ru>
2022-06-23 01:29:49 +0300

    fix_tests

--- test/lib/hexlet_code_test.rb
+++ test/lib/hexlet_code_test.rb
@@ -7,36 +7,36 @@ class HexletCodeTest < Minitest::Test
     constant_user = Struct.new(:name, :job, :gender, keyword_init: true)
     @user1 = constant_user.new name: 'rob', job: 'hexlet', gender: 'm'
     @user2 = constant_user.new job: 'hexlet'
-    @form1 = load_fixtures('form1.html')
-    @form2 = load_fixtures('form2.html')
-    @form3 = load_fixtures('form3.html')
   end
 
   def test_default_attr
+    expected = load_fixtures('form1.html')
     html = HexletCode.form_for @user1 do |f|
       f.input :name
       f.input :job, as: :text
       f.submit
     end
-    assert_equal(@form1, html)
+    assert_equal(expected, html)
   end
 
   def test_custom_attr
+    expected = load_fixtures('form2.html')
     html = HexletCode.form_for @user1, url: '/users' do |f|
       f.input :name, class: 'user-input'
       f.input :job, as: :text, rows: 50, cols: 50
       f.submit
     end
-    assert_equal(@form2, html)
+    assert_equal(expected, html)
   end
 
   def test_create_custom_submit
+    expected = load_fixtures('form3.html')
     html = HexletCode.form_for @user2 do |f|
       f.input :name
       f.input :job
       f.submit 'Go', name: 'registratio'
     end
-    assert_equal(@form3, html)
+    assert_equal(expected, html)
   end
 
   def test_not_exists_user_method
